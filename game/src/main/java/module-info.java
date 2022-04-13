924ec6e9cab30c21655374fe7298d5a9e6f50ff7
kirth123 <88990184+kirth123@users.noreply.github.com>
2022-04-12 21:46:17 -0400

    Delete game directory

--- game/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.reddy.game {
-    requires javafx.controls;
-    requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
-
-    opens com.reddy.game to javafx.fxml;
-    exports com.reddy.game;
-}
